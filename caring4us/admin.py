802adb3494b14198c5537a0889c3fb7967a3e468
Chris Beldam <chrisgbeldam@gmail.com>
2017-08-22 20:47:30 +0100

    Bootstrap 4 Beta

--- caring4us/admin.py
+++ caring4us/admin.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Illness, Carers
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Illness)
+admin.site.register(Carers)
--- caring4us/models.py
+++ caring4us/models.py
@@ -9,12 +9,17 @@ class Person(models.Model):
     name = models.CharField(max_length=200, primary_key = True)
     age = models.IntegerField()
     gender = models.CharField(max_length=200)
-
+    carer = models.CharField(max_length=200)
     def __str__(self):
         return self.name
 
 class Illness(models.Model):
     illness = models.CharField(max_length=200, primary_key = True)
-    severity = models.CharField(max_length=200, primary_key = True)
+    severity = models.CharField(max_length=200)
     medication = models.BooleanField()
-    treatment_plan = models.TextField()
+
+class Carers(models.Model):
+    name = models.CharField(max_length=200, primary_key = True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=200)
+    give_car_to = models.ForeignKey(Person)
